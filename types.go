b317ed135beec21c0c0e4b925c04a0e33f3cf960
Leonid Belikov <leonid@belikov.net>
2018-05-02 19:41:51 +0300

    globals/types

---
+++ types.go
@@ -0,0 +1,32 @@
+package main
+
+type Config struct {
+	Token         string		`json:"BOT_TOKEN"`
+	Extension     string		`json:"STORAGE_EXT"`
+}
+
+type Storage struct {
+	ID            string		`json:"id"`
+	Title         string		`json:"title"`
+	Login         string		`json:"login"`
+	Pass          string		`json:"pass"`
+	Url           string		`json:"url"`
+}
+
+type Current struct {
+	Command       string
+	Step          int
+	Entry         Storage
+	Element       int
+	Continuous    bool
+}
+
+type Output struct {
+	Response      string
+	Buttons       []InlineButton
+}
+
+type InlineButton struct {
+	Text          string
+	URL           string
+}
